4fe89e4accfa80da5d49bfee397c3e78606322a6
tacobayle <nicolas.bayle@gmail.com>
2021-12-03 10:20:57 +0000

    20211203_avi_username

--- 01_infra/output.tf
+++ 01_infra/output.tf
@@ -3,3 +3,15 @@
 output "controller_ip_dhcp_cluster" {
   value = var.dhcp == true && var.avi_cluster== true ? vsphere_virtual_machine.controller_dhcp_cluster.*.default_ip_address : null
 }
+
+output "controller_ip_dhcp_standalone" {
+  value = var.dhcp == true && var.avi_cluster== false ? vsphere_virtual_machine.controller_dhcp_standalone.*.default_ip_address : null
+}
+
+output "controller_ip_static_cluster" {
+  value = var.dhcp == false && var.avi_cluster== true ? vsphere_virtual_machine.controller_static_cluster.*.default_ip_address : null
+}
+
+output "controller_ip_static_standalone" {
+  value = var.dhcp == false && var.avi_cluster== false ? vsphere_virtual_machine.controller_static_standalone.*.default_ip_address : null
+}
