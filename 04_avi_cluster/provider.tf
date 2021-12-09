960b8c647fb6babdae2bb0893074beab18a97f74
tacobayle <nicolas.bayle@gmail.com>
2021-12-09 21:24:21 +0000

    20211209_avi_config

--- 04_avi_cluster/provider.tf
+++ 04_avi_cluster/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     avi = {
       source  = "vmware/avi"
-      version = "21.1.2"
+      version = version_to_be_replaced
     }
   }
 }
--- apply.sh
+++ apply.sh
@@ -3,22 +3,77 @@
 # Orchestrate TF apply
 #
 cd 01_infra
-terraform init
-terraform apply -auto-approve
+terraform init > init.stdout 2> init.stderr
+if [ -s init.stderr ] ; then
+    echo "TF Init ERRORS:"
+    cat init.stderr
+    exit 255
+fi
+terraform apply -auto-approve > apply.stdout 2> apply.stderr
+if [ -s apply.stderr ] ; then
+    echo "TF Apply ERRORS:"
+    cat apply.stderr
+    exit 255
+fi
 cd -
 #
 cd 02_avi_username
-terraform init
-terraform apply -auto-approve -var-file=../controllers.json -var-file=../avi_config.json -compact-warnings
+if [ -z "$TF_VAR_avi_version" ]; then
+  sed -i -e 's/version_to_be_replaced/"21.1.2"/g' provider.tf
+else
+  sed -i -e "s/version_to_be_replaced/\"$TF_VAR_avi_version\"/g" provider.tf
+fi
+terraform init > init.stdout 2> init.stderr
+if [ -s init.stderr ] ; then
+    echo "TF Init ERRORS:"
+    cat init.stderr
+    exit 255
+fi
+terraform apply -auto-approve -var-file=../controllers.json -var-file=../avi_config.json -compact-warnings > apply.stdout 2> apply.stderr
+if [ -s apply.stderr ] ; then
+    echo "TF Apply ERRORS:"
+    cat apply.stderr
+    exit 255
+fi
 cd -
 #
 cd 03_avi_config
-terraform init
-terraform apply -auto-approve -var-file=../controllers.json -var-file=../avi_config.json -var-file=../.password.json -compact-warnings
+if [ -z "$TF_VAR_avi_version" ]; then
+  sed -i -e 's/version_to_be_replaced/"21.1.2"/g' provider.tf
+else
+  sed -i -e "s/version_to_be_replaced/\"$TF_VAR_avi_version\"/g" provider.tf
+fi
+terraform init > init.stdout 2> init.stderr
+if [ -s init.stderr ] ; then
+    echo "TF Init ERRORS:"
+    cat init.stderr
+    exit 255
+fi
+terraform apply -auto-approve -var-file=../controllers.json -var-file=../avi_config.json -var-file=../.password.json -compact-warnings > apply.stdout 2> apply.stderr
+if [ -s apply.stderr ] ; then
+    echo "TF Apply ERRORS:"
+    cat apply.stderr
+    exit 255
+fi
 cd -
 #
 #cd 04_avi_cluster
-#terraform init
-#terraform apply -auto-approve -var-file=../controllers.json -var-file=../avi_config.json -var-file=../.password.json
+#if [ -z "$TF_VAR_avi_version" ]; then
+#  sed -i -e 's/version_to_be_replaced/"21.1.2"/g' provider.tf
+#else
+#  sed -i -e "s/version_to_be_replaced/\"$TF_VAR_avi_version\"/g" provider.tf
+#fi
+#terraform init > init.stdout 2> init.stderr
+#if [ -s init.stderr ] ; then
+#    echo "TF Init ERRORS:"
+#    cat init.stderr
+#    exit 255
+#fi
+#terraform apply -auto-approve -var-file=../controllers.json -var-file=../avi_config.json -var-file=../.password.json > apply.stdout 2> apply.stderr
+#if [ -s apply.stderr ] ; then
+#    echo "TF Apply ERRORS:"
+#    cat apply.stderr
+#    exit 255
+#fi
 #cd -
 #
