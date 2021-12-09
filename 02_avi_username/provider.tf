082c009fa8c9db4dad211e74785d7947250fde84
tacobayle <nicolas.bayle@gmail.com>
2021-12-09 20:50:18 +0000

    20211207_avi_config

--- 02_avi_username/provider.tf
+++ 02_avi_username/provider.tf
@@ -12,4 +12,6 @@ provider "avi" {
   avi_password   = var.avi_current_password
   avi_controller = var.avi_controller_ips[0]
   avi_tenant     = var.avi_tenant
+  avi_version    = var.avi_version
+
 }
